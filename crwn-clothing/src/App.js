2e22bc087d6c1f52af51609413fefbf7ee8da5fe
JatinAcharya <jatinacharya29@gmail.com>
2020-04-15 14:00:10 +0530

    sign in form frontend done

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
 
 import './App.css';
@@ -13,6 +14,7 @@ function App() {
       <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
         <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
